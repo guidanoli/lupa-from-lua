e54b2093798065757eab057e1131d1727a2b4789
guidanoli <guidanoli@hotmail.com>
2020-11-24 18:27:00 -0300

    Skeleton for test framework

--- tests/test.lua
+++ tests/test.lua
@@ -4,44 +4,24 @@
 
 package.cpath = package.cpath .. ";./lib/?.so"
 
-local python, libpath = require('lupafromlua')
+local python = assert(require("lupafromlua"))
 
-if python then
-	print("lupafromlua successfully loaded from "..libpath)
-else
-	error("Could not load lupafromlua")
+Testbench = {}
+
+function Testbench:TestSomethingTrue()
+	assert(true)
 end
 
-local builtins = python.as_attrgetter(python.builtins).__dict__
-
-local list = builtins.list()
-
-print()
-print("Empty list:")
-print(list)
-
-for i = 1, 10 do
-	python.as_attrgetter(list).append(i)
+function Testbench:TestSomethingFalse()
+	assert(false)
 end
 
-print()
-print("List with numbers from 1 to 10:")
-print(list)
-
-local dict = builtins.dict()
-
-print()
-print("Empty dictionary:")
-print(dict)
-
-for i = 1, 10 do
-	dict[i] = i*i
+print("Running tests...")
+for testcase, testfunc in pairs(Testbench) do
+	local ok, errmsg = pcall(testfunc, Testbench)
+	if ok then
+		print(testcase, "ok")
+	else
+		print(testcase, errmsg)
+	end
 end
-
-print()
-print("Dictionary mapping x to x^2 for x from 1 to 10:")
-print(dict)
-
-lua = python.eval('lua')
-
-print(lua.eval('1 + 1'))
