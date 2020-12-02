8c1275eab2622c305559ea6a23853b1bd3a3e4a3
guidanoli <guidanoli@hotmail.com>
2020-12-02 11:39:45 -0300

    CMake builds Lupa automatically

--- buildlupa.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd lupa
-rm -f lupa/_lupa.c
-pip install -r requirements.txt
-python setup.py build
-cd ..
--- setuplupa.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-export PYTHONPATH=`find lupa -maxdepth 2 -type d -regex "lupa/build/lib.*"`
