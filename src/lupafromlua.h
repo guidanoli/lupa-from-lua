769a61078289bcc629635d10feb36b944c74e49a
guidanoli <guidanoli@hotmail.com>
2021-02-13 10:47:34 -0300

    Refactored header file

--- src/lupafromlua.h
+++ src/lupafromlua.h
@@ -13,21 +13,17 @@
 #endif
 
 #if defined(_MSC_VER)
-    /* Microsoft */
-    #define DLL_EXPORT __declspec(dllexport)
-    #define DLL_IMPORT __declspec(dllimport)
+#	define DLL_EXPORT __declspec(dllexport)
+#	define DLL_IMPORT __declspec(dllimport)
 #elif defined(__GNUC__)
-    /* GCC */
-    #define DLL_EXPORT __attribute__((visibility("default")))
-    #define DLL_IMPORT
+#	define DLL_EXPORT __attribute__((visibility("default")))
+#	define DLL_IMPORT
 #else
-    /* do nothing and hope for the best? */
-    #define DLL_EXPORT
-    #define DLL_IMPORT
-    #error "Unknown dynamic link import/export semantics."
+#	define DLL_EXPORT
+#	define DLL_IMPORT
+#	error Unknown dynamic link import/export semantics.
 #endif
 
-/* Define the same function for obtaining the version of Lua */
 #if LUA_VERSION_NUM >= 504
 #	define read_lua_version(L)  ((long int) lua_version(L))
 #elif LUA_VERSION_NUM >= 502
